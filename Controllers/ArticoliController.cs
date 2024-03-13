40a468b542d51ed88c169f8a489b76076e1a65b8
Alessandro Basile <alessandrobasile1992@gmail.com>
2024-03-13 14:53:25 +0100

    need to fix authorization roles

--- Controllers/ArticoliController.cs
+++ Controllers/ArticoliController.cs
@@ -11,28 +11,23 @@ namespace back_end_s7.Controllers
     {
         private FornoDbContext dbContext = new FornoDbContext();
 
-        // GET: Articoli
         public ActionResult Index()
         {
-            // Recupera tutti gli articoli dal database
             var articoli = dbContext.Articoli.ToList();
             return View(articoli);
         }
 
-        // GET: Articoli/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: Articoli/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create(Articoli articolo)
         {
             if (ModelState.IsValid)
             {
-                // Aggiungi il nuovo articolo al database
                 dbContext.Articoli.Add(articolo);
                 dbContext.SaveChanges();
                 return RedirectToAction("Index");
@@ -41,22 +36,18 @@ namespace back_end_s7.Controllers
             return View(articolo);
         }
 
-        // GET: Articoli/Edit/5
         public ActionResult Edit(int id)
         {
-            // Recupera l'articolo dal database
             var articolo = dbContext.Articoli.Find(id);
             return View(articolo);
         }
 
-        // POST: Articoli/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit(Articoli articolo)
         {
             if (ModelState.IsValid)
             {
-                // Aggiorna l'articolo nel database
                 dbContext.Entry(articolo).State = System.Data.Entity.EntityState.Modified;
                 dbContext.SaveChanges();
                 return RedirectToAction("Index");
@@ -65,20 +56,16 @@ namespace back_end_s7.Controllers
             return View(articolo);
         }
 
-        // GET: Articoli/Delete/5
         public ActionResult Delete(int id)
         {
-            // Recupera l'articolo dal database
             var articolo = dbContext.Articoli.Find(id);
             return View(articolo);
         }
 
-        // POST: Articoli/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
-            // Trova e rimuovi l'articolo dal database
             var articolo = dbContext.Articoli.Find(id);
             dbContext.Articoli.Remove(articolo);
             dbContext.SaveChanges();
